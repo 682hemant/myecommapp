00fe131d758cffcbe62ea6795d946fe9cc2ae4a4
682hemant <premprataps1998@gmail.com>
2020-09-14 17:20:52 +0530

    last commit

--- src/context.js
+++ src/context.js
@@ -1,121 +1,171 @@
 import React, { Component } from 'react';
-import  {storeProducts,detailProduct} from './data';
+import { storeProducts, detailProduct } from './data';
 const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
-  state={
-    products:[],
-    detailProduct:detailProduct,
-    cart:[],
-    modalOpen:false,
-    modalProduct:detailProduct,
-    cartSubTotal:0,
-    cartTax:0,
-    cartTotal:0
+  state = {
+    products: [],
+    detailProduct: detailProduct,
+    cart: [],
+    modalOpen: false,
+    modalProduct: detailProduct,
+    cartSubTotal: 0,
+    cartTax: 0,
+    cartTotal: 0
   };
-  componentDidMount(){
+  componentDidMount() {
     this.setProducts();
   }
-  setProducts=()=>{
-    let tempProducts =[];
-    storeProducts.forEach(item=>{
-      const singleItem ={...item};
-      tempProducts =[...tempProducts,singleItem];
+  setProducts = () => {
+    let tempProducts = [];
+    storeProducts.forEach(item => {
+      const singleItem = { ...item };
+      tempProducts = [...tempProducts, singleItem];
     })
     console.log(tempProducts)
-    this.setState(()=>{
-         return {products:tempProducts}
+    this.setState(() => {
+      return { products: tempProducts }
     })
   }
-  getItem=(id)=>{
-    const product =this.state.products.find(item=>item.id===id);
+  getItem = (id) => {
+    const product = this.state.products.find(item => item.id === id);
     return product;
   }
-  handleDetail=(id)=>{
-     const product =this.getItem(id);
-     this.setState(()=>{
-         return {
-           detailProduct:product
-         }
-     })
-  }
-  addToCart=id=>{
-    let tempProducts = [...this.state.products];
-     const index = tempProducts.indexOf(this.getItem(id));
-     const product = tempProducts[index];
-     product.inCart=true;
-     product.count=1;
-     const price =product.price;
-     product.total =price;
-     this.setState(()=>{
-          return {products:tempProducts,cart:[...this.state.cart,product]}
-     },()=>{this.addTotals()})
-  }
-  openModal=id=>{
+  handleDetail = (id) => {
     const product = this.getItem(id);
-    this.setState(()=>{
-      return{
-        modalProduct:product,
-        modalOpen:true
+    this.setState(() => {
+      return {
+        detailProduct: product
       }
     })
   }
-  closeModal=()=>{
-    this.setState(()=>{
-      return{
-        modalOpen:false
+  addToCart = id => {
+    let tempProducts = [...this.state.products];
+    const index = tempProducts.indexOf(this.getItem(id));
+    const product = tempProducts[index];
+    product.inCart = true;
+    product.count = 1;
+    const price = product.price;
+    product.total = price;
+    this.setState(() => {
+      return { products: tempProducts, cart: [...this.state.cart, product] }
+    }, () => { this.addTotals() })
+  }
+  openModal = id => {
+    const product = this.getItem(id);
+    this.setState(() => {
+      return {
+        modalProduct: product,
+        modalOpen: true
       }
     })
   }
-  increment=(id)=>{
-    console.log('this is increament method');
+  closeModal = () => {
+    this.setState(() => {
+      return {
+        modalOpen: false
+      }
+    })
   }
-  decrement=(id)=>{
-    console.log('this is dcreament method');
-  }
-  removeItem=(id)=>{
-    console.log('item removed')
-  }
-  clearCart=()=>{
-    console.log('cart was cleared')
-  }
-  addTotals=()=>{
-     let subTotal = 0;
-     this.state.cart.map(item=>(subTotal+=item.total));
-     const tempTax=subTotal*0.1;
-     const tax = parseFloat(tempTax.toFixed(2));
-     const total =subTotal+tax;
-     this.setState(()=>{
-       return{
-         cartSubTotal:subTotal,
-          cartTax:tax,
-          cartTotal:total
-       }
-     })
+  increment = (id) => {
+    let tempCart = [...this.state.cart];
+    const selectedProduct = tempCart.find(item => item.id === id)
+    const index = tempCart.indexOf(selectedProduct);
+    const product = tempCart[index];
+    product.count = product.count + 1;
+    product.total = product.count * product.price;
+    this.setState(() => {
+      return {
+        cart: [...tempCart]
+      }
+    }, () => { this.addTotals() })
 
+  }
+  decrement = id => {
+    let tempCart = [...this.state.cart];
+    const selectedProduct = tempCart.find(item => item.id === id)
 
+    const index = tempCart.indexOf(selectedProduct);
+    const product = tempCart[index];
+    product.count = product.count - 1;
+    if (product.count === 0) {
+      this.removeItem(id)
+    }
+    else {
+      product.total = product.count * product.price;
+      this.setState(() => {
+        return {
+          cart: [...tempCart]
+        }
+      }, () => { this.addTotals() })
+
+    }
+  }
+  removeItem = (id) => {
+    let tempProducts = [...this.state.products];
+    let tempCart = [...this.state.cart];
+    tempCart = tempCart.filter(item => item.id !== id);
+    const index = tempProducts.indexOf(this.getItem(id));
+    let removedProduct = tempProducts[index];
+    removedProduct.inCart = false;
+    removedProduct.count = 0;
+    removedProduct.total = 0;
+    this.setState(() => {
+      return {
+        cart: [...tempCart],
+        products: [...tempProducts]
+
+      }
+    }, () => {
+      this.addTotals();
+    })
+
+  }
+  clearCart = () => {
+    this.setState(() => {
+      return {
+        cart: []
+      }
+    }, () => {
+      this.setProducts();
+      this.addTotals();
+    })
+  }
+  addTotals = () => {
+    let subTotal = 0;
+    this.state.cart.map(item => (subTotal += item.total));
+    const tempTax = subTotal * 0.1;
+    const tax = parseFloat(tempTax.toFixed(2));
+    const total = subTotal + tax;
+    this.setState(() => {
+      return {
+        cartSubTotal: subTotal,
+        cartTax: tax,
+        cartTotal: total
+      }
+    })
   }
 
 
   render() {
     return (
       <ProductContext.Provider value={{
-          ...this.state,
-          handleDetail:this.handleDetail,
-          addToCart:this.addToCart,
-           openModal:this.openModal,
-           closeModal:this.closeModal,
-           increment:this.increment,
-           decrement:this.decrement,
-           removeItem:this.removeItem,
-           clearCart:this.clearCart
+        ...this.state,
+        handleDetail: this.handleDetail,
+        addToCart: this.addToCart,
+        openModal: this.openModal,
+        closeModal: this.closeModal,
+        increment: this.increment,
+        decrement: this.decrement,
+        removeItem: this.removeItem,
+        clearCart: this.clearCart
       }}>
-       {this.props.children}
+        {this.props.children}
       </ProductContext.Provider>
     )
   }
 }
 
-const ProductConsumer =ProductContext.Consumer;
+const ProductConsumer = ProductContext.Consumer;
 
-export {ProductConsumer,ProductProvider};
+export { ProductConsumer, ProductProvider };
--- src/data.js
+++ src/data.js
@@ -2,6 +2,42 @@
 export const storeProducts = [
   {
     id: 1,
+    title: "moto  e6- Black",
+    img: "img/E6.jpeg",
+    price: 6,
+    company: "MOto",
+    info:
+      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    inCart:false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "moto c plus- Black",
+    img: "img/Moto-C.webp",
+    price: 6,
+    company: "MOto",
+    info:
+      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    inCart:false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "j7- Black",
+    img: "img/j7.jpeg",
+    price: 6,
+    company: "SVMSUNG",
+    info:
+      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    inCart:false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
     title: "Google Pixel - Black",
     img: "img/product-1.png",
     price: 10,
@@ -13,7 +49,7 @@ export const storeProducts = [
     total: 0
   },
   {
-    id: 2,
+    id: 5,
     title: "Samsung S7",
     img: "img/product-2.png",
     price: 16,
@@ -25,7 +61,7 @@ export const storeProducts = [
     total: 0
   },
   {
-    id: 3,
+    id: 6,
     title: "HTC 10 - Black",
     img: "img/product-3.png",
     price: 8,
@@ -37,7 +73,7 @@ export const storeProducts = [
     total: 0
   },
   {
-    id: 4,
+    id: 7,
     title: "HTC 10 - White",
     img: "img/product-4.png",
     price: 18,
@@ -49,7 +85,7 @@ export const storeProducts = [
     total: 0
   },
   {
-    id: 5,
+    id: 8,
     title: "HTC Desire 626s",
     img: "img/product-5.png",
     price: 24,
@@ -61,7 +97,7 @@ export const storeProducts = [
     total: 0
   },
   {
-    id: 6,
+    id: 9,
     title: "Vintage Iphone",
     img: "img/product-6.png",
     price: 17,
@@ -73,7 +109,7 @@ export const storeProducts = [
     total: 0
   },
   {
-    id: 7,
+    id: 10,
     title: "Iphone 7",
     img: "img/product-7.png",
     price: 30,
@@ -85,7 +121,7 @@ export const storeProducts = [
     total: 0
   },
   {
-    id: 8,
+    id: 11,
     title: "Smashed Iphone",
     img: "img/product-8.png",
     price: 2,
@@ -97,7 +133,7 @@ export const storeProducts = [
     total: 0
   },
   {
-      id: 9,
+      id: 12,
       title: "Smashed Iphone",
       img: "img/product-8.png",
       price: 2,
@@ -109,7 +145,7 @@ export const storeProducts = [
       total: 0
   },
   {
-    id: 10,
+    id: 13,
     title: "Smashed Iphone",
     img: "img/product-8.png",
     price: 2,
@@ -121,7 +157,7 @@ export const storeProducts = [
     total: 0
 },
 {
-  id: 11,
+  id: 14,
   title: "Smashed Iphone",
   img: "img/product-8.png",
   price: 2,
@@ -133,7 +169,7 @@ export const storeProducts = [
   total: 0
 },
 {
-  id: 12,
+  id: 15,
   title: "Smashed Iphone",
   img: "img/product-8.png",
   price: 2,
@@ -145,7 +181,7 @@ export const storeProducts = [
   total: 0
 },
 {
-  id: 13,
+  id: 16,
   title: "Google Pixel - Black",
   img: "img/product-1.png",
   price: 10,
@@ -157,7 +193,7 @@ export const storeProducts = [
   total: 0
 },
 {
-  id: 14,
+  id: 17,
   title: "Samsung S7",
   img: "img/product-2.png",
   price: 16,
@@ -169,7 +205,7 @@ export const storeProducts = [
   total: 0
 },
 {
-  id: 15,
+  id: 18,
   title: "HTC 10 - Black",
   img: "img/product-3.png",
   price: 8,
@@ -181,7 +217,7 @@ export const storeProducts = [
   total: 0
 },
 {
-  id: 16,
+  id: 19,
   title: "HTC 10 - White",
   img: "img/product-4.png",
   price: 18,
@@ -193,7 +229,7 @@ export const storeProducts = [
   total: 0
 },
 {
-  id: 17,
+  id: 20,
   title: "narzo 10 A",
   img: "img/narzo.jpeg",
   price: 25,
@@ -205,7 +241,7 @@ export const storeProducts = [
   total: 0
 },
 {
-  id: 18,
+  id: 21,
   title: "Realeme 5i",
   img: "img/reale5i.jpeg",
   price: 150,
